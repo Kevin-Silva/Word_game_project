c62a83b8da8d5a527e122873eaaa7c05a6f33370
kevin.silva <kevin.t.silva@protonmail.com>
2023-12-24 20:47:25 -0300

    feature: setup the api for getting the word.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const API_URL = ""
+const API_URL = "https://words.dev-apis.com/word-of-the-day"
 
+async function getWordOfTheDay(){
+    const promise = await fetch(API_URL, {
+        method: "GET"
+    });
+    const processedResponse = await promise.json();
+    return processedResponse;
+}
 function isLetter(letter) {
     return /^[a-zA-Z]$/.test(letter);
-  }
+}
+
+getWordOfTheDay();
